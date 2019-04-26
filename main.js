91ce8760546ec01a1afad6b15f024b5c17a0d2fe
Ishaan <ishaanpaul98@gmail.com>
2019-04-26 11:21:16 -0400

    editing js

---
+++ main.js
@@ -0,0 +1,3 @@
+var pieces, radius, fft, mapMouseX, mapMouseY, toggleBtn, audio, uploadBtn, uploadedAudio, uploadAnim;
+var colorPalette = ["#000", "rgba(22, 59, 72, 0.5)", "#00a6e0", "#002a38"];
+var uploadLoading = false;
